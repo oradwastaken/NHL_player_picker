1d5ab17b89bb56844b1c5e82180066d25f009f46
Orad Reshef <orad@reshef.ca>
2021-10-02 13:53:44 -0400

    Updating it so Rob sees the latest progress

--- hockey_stuff.py
+++ hockey_stuff.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum, auto
 from datetime import date
+from typing import Optional
 
 
 class Position(Enum):
@@ -14,6 +15,26 @@ class Position(Enum):
         return self.name
 
 
+@dataclass
+class SkaterStats:
+    G: int
+    A: int
+    Blk: int
+    Hit: int
+
+    @property
+    def Pts(self):
+        return self.G + self.A
+
+
+@dataclass
+class GoalieStats:
+    W: int
+    L: int
+    GAA: float
+    SVPcT: float
+    SO: int
+
 
 @dataclass
 class Player:
@@ -22,6 +43,9 @@ class Player:
     DOB: date
     ELO_rating: float
     position: Position
+    GP: int
+    skater_stats: Optional[SkaterStats] = None
+    goalie_stats: Optional[GoalieStats] = None
 
     def __str__(self):
         return f'{self.name} ({self.position})'
@@ -32,7 +56,8 @@ class Player:
         today = date.today()
         return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
-    def win(self, other_player):
+    def set_rating(self, new_rating):
+        self.ELO_rating = new_rating
+
+    def win_against(self, other_player):
         pass
-
-
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def main():
-    from hockey_stuff import Player, Position
+    from hockey_stuff import Player, Position, SkaterStats, GoalieStats
     from datetime import date
 
-    player1 = Player('Nikita Kucherov', 'Tampa Bay Lightning', date(1993, 6, 17), 1000, Position.RW)
-    player2 = Player('Nikita Kucherov', 'Tampa Bay Lightning', date(1993, 6, 17), 1000, Position.RW)
+    player1 = Player('Brayden Point', 'Tampa Bay Lightning', date(1996, 3, 13), 1000, Position.RW, 56,
+                     SkaterStats(G=23, A=25, Blk=18, Hit=23))
+    player2 = Player('Victor Olofsson', 'Buffalo Sabres', date(1995, 7, 18), 1000, Position.RW, 56,
+                     SkaterStats(G=13, A=19, Blk=12, Hit=17))
 
+    print(player1.skater_stats)
+    # print(player2.G)
 
 
 if __name__ == '__main__':
