41d6ac4bbe220b81279ea5af309c9aea175fab48
Orad Reshef <orad@reshef.ca>
2021-10-04 19:36:00 -0400

    Implemented ELO ranking

--- hockey_stuff.py
+++ hockey_stuff.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from enum import Enum, auto
 from datetime import date
 from typing import Optional, Union
+from random import sample
 
 
 class Position(Enum):
@@ -47,7 +48,7 @@ class Player:
     stats: Union[SkaterStats, GoalieStats]
 
     def __str__(self):
-        return f'{self.name} ({self.position}) ELO:{self.ELO_rating}'
+        return f'{self.name} ({self.position}) ELO:{self.ELO_rating:.2f}'
 
     @property
     def age(self):
@@ -59,13 +60,14 @@ class Player:
         self.ELO_rating = new_rating
 
 
-@dataclass()
 class Rankings:
+    ELO_k = 40
+
     def __init__(self):
         self.player_list = []
 
     def __str__(self):
-        self.player_list.sort(key=lambda player: player.ELO_rating)
+        self.player_list.sort(key=lambda player_temp: player_temp.ELO_rating)
 
         ranking_str = ''
         for i, player in enumerate(self.player_list):
@@ -75,13 +77,35 @@ class Rankings:
     def add_player(self, new_player: Player):
         self.player_list.append(new_player)
 
-    def matchup(self, player1, player2):
-        print('Who would you rather have on your team? Player #1 or #2?')
+    def random_matchup(self):
+        player1, player2 = sample(self.player_list, 2)
+        R1 = 10 ** (player1.ELO_rating / 400)
+        R2 = 10 ** (player2.ELO_rating / 400)
+        E1 = R1 / (R1 + R2)
+        E2 = R2 / (R1 + R2)
+
         print(f'#1: {player1}')
         print(f'#2: {player2}')
-        while (userinput := int(input('[1/2]?'))) not in [1, 2]:
-            print('Please type 1 or 2')
-        if userinput == 1:
+        print(f'#3: TIE!')
+        print('Who would you rather have on your team? Player #1 or #2?')
+        while (userinput := input('(1/2/3)? ')) not in ['1', '2', '3']:
+            print('Please type 1, 2 or 3:')
+
+        if userinput == '1':
             print(f'{player1.name} wins')
-        if userinput == 2:
+            S1, S2 = 1, 0
+
+        if userinput == '2':
             print(f'{player2.name} wins')
+            S1, S2 = 1/2, 1/2
+
+        if userinput == '3':
+            print(f"It's a tie!")
+            S1, S2 = 0, 1
+
+        player1.set_rating(player1.ELO_rating + self.ELO_k * (S1 - E1))
+        player2.set_rating(player2.ELO_rating + self.ELO_k * (S2 - E2))
+
+        print("\nNew values:")
+        print(player1)
+        print(player2)
--- main.py
+++ main.py
@@ -2,16 +2,15 @@ def main():
     from hockey_stuff import Player, Position, SkaterStats, GoalieStats, Rankings
     from datetime import date
 
-    player1 = Player('Brayden Point', 'Tampa Bay Lightning', date(1996, 3, 13), 1100, Position.RW, 56,
-                     SkaterStats(G=23, A=25, Blk=18, Hit=23))
-    player2 = Player('Victor Olofsson', 'Buffalo Sabres', date(1995, 7, 18), 1000, Position.RW, 56,
-                     SkaterStats(G=13, A=19, Blk=12, Hit=17))
-
     player_rankings = Rankings()
-    player_rankings.add_player(player1)
-    player_rankings.add_player(player2)
+    player_rankings.add_player(Player('Brayden Point', 'Tampa Bay Lightning', date(1996, 3, 13), 1100, Position.RW, 56,
+                                      SkaterStats(G=23, A=25, Blk=18, Hit=23)))
+    player_rankings.add_player(Player('Victor Olofsson', 'Buffalo Sabres', date(1995, 7, 18), 1000, Position.RW, 56,
+                                      SkaterStats(G=13, A=19, Blk=12, Hit=17)))
+    player_rankings.add_player(Player('Carey Price', 'Montreal Canadiens', date(1987, 8, 16), 1000, Position.G, 25,
+                                      GoalieStats(W=12, L=7, GAA=2.64, SVPcT=.901, SO=1)))
 
-    player_rankings.matchup(player1, player2)
+    player_rankings.random_matchup()
 
 
 if __name__ == '__main__':
